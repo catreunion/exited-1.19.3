ef95e2e65f421a9eb5e76a4a6b7ecc947d7a4d66
catreunion <isaacli@mac.com>
2023-03-26 17:00:10 +0800

    version 1.0.0

---
+++ src/main/java/net/exited/tutorialmod/TutorialMod.java
@@ -0,0 +1,20 @@
+package net.exited.tutorialmod;
+
+import net.exited.tutorialmod.block.ModBlocks;
+import net.exited.tutorialmod.item.ModItemGroup;
+import net.exited.tutorialmod.item.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TutorialMod implements ModInitializer {
+	public static final String MOD_ID = "tutorialmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger("tutorialmod");
+
+	@Override
+	public void onInitialize() {
+		ModItemGroup.registerItemGroups();
+		ModItems.registerModItems();
+		ModBlocks.registerModBlocks();
+	}
+}
